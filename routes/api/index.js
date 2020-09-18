5185fa1412272dabbca8bba87fdb878cb839548c
Moo Point <beachgal0105@gmail.com>
2020-09-17 21:08:16 -0700

    api index

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
-const thoughtRoutes = require('./thought-routes');
 const userRoutes = require('./user-routes');
-
+const thoughtRoutes = require('./thought-routes');
 // add prefix of `/users` to routes created in `user-routes.js`
-router.use('/thoughts', thoughtRoutes);
 router.use('/users', userRoutes);
-
+router.use('/thoughts', thoughtRoutes);
 module.exports = router;
